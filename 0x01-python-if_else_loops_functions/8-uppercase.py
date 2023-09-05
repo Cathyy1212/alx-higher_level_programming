6853929b35f3cd51af84c3b86fe71bcc0d2606f6
Cathyy1212 <khadijahissou2@gmail.com>
2023-09-05 11:19:07 -0700

    if else loops

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,7 @@
 # convert_to_uppercase.py
 def uppercase(input_str):
     for char in input_str:
+        # Check if the character is lowercase (ASCII range 97-122)
         if ord(char) >= 97 and ord(char) <= 122:
             uppercase_char = chr(ord(char) - 32)
         else:
@@ -9,4 +10,6 @@ def uppercase(input_str):
         print(uppercase_char, end="")
     print()  # Print a new line after the loop
 
-
+# Test the function with your examples
+uppercase("best")
+uppercase("Best School 98 Battery street")
