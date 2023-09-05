cb4190da0800945d20fdb7a3b5688f7c621311db
Cathyy1212 <khadijahissou2@gmail.com>
2023-09-05 11:33:23 -0700

    fix doublon

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 # print_and_return_last_digit.py
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print(last_digit, end="")  # Imprime la dernière chiffre sans nouvelle ligne
+    return last_digit  # Retourne la dernière chiffre
 
-def print_and_return_last_digit(input_number):
-    """Print the last digit of a number and return it."""
-    last_digit = abs(input_number) % 10
-    print(last_digit, end="")
-    return last_digit
+# Testez la fonction avec vos exemples
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print()  # Imprime une nouvelle ligne pour le dernier résultat
+print(r)  # Imprime la dernière chiffre stockée dans la variable 'r'
