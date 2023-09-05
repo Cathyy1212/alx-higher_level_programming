2b116dbef96b62e78d26b397b4b5ff12976b6233
Cathyy1212 <khadijahissou2@gmail.com>
2023-09-05 15:14:35 -0700

    10 add

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # calculate_sum.py
 
-def calculate_sum(x, y):
-    """Return the sum of x and y."""
-    return (x + y)
+add = __import__('10-add').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
+
