66354b33e9a977d52c7e9646ebad9178f293be5a
Cathyy1212 <khadijahissou2@gmail.com>
2023-09-05 11:39:50 -0700

    fix doublon

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 # print_and_return_last_digit.py
-def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit, end="")  # Imprime la dernière chiffre sans nouvelle ligne
-    return last_digit  # Retourne la dernière chiffre
-
-# Testez la fonction avec vos exemples
+# main_0.py
 print_last_digit(98)
 print_last_digit(0)
-r = print_last_digit(-1024)
-print()  # Imprime une nouvelle ligne pour le dernier résultat
-print(r)  # Imprime la dernière chiffre stockée dans la variable 'r'
+r = print_last_digit(-98)
+print(r)
+
+r = print_last_digit(98)
+print(r + print_last_digit(0))
+r = print_last_digit(-98)
+print(r + r)
+
+r = print_last_digit(0)
+print(r + r)
+
+try:
+    print_last_digit("Holberton")
+except TypeError as e:
+    print(e)
+
+def print_last_digit(number):
+    if isinstance(number, int):
+        last_digit = abs(number) % 10
+        print(last_digit, end="")
+        return last_digit
+    else:
+        raise TypeError("Input must be an integer")
