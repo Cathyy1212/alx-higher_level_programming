4a354b01c5b313af1cb2d5328dad5f94cc986b44
Cathyy1212 <khadijahissou2@gmail.com>
2023-09-05 16:26:11 -0700

    10 add

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # calculate_power.py
 
-def calculate_power(base, exponent):
+def pow(base, exponent):
     """Return the result of raising 'base' to the power of 'exponent'."""
     return (base ** exponent)
