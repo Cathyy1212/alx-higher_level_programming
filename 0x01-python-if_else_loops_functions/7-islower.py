7cc56f894a3e2a246160c118b3be2bb24925015e
Cathyy1212 <khadijahissou2@gmail.com>
2023-09-05 11:00:23 -0700

    if else loops

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 # is_lowercase_character.
 def islower(c):
-    # Check if the ASCII value of the character is in the lowercase range (97 to 122)
     return ord(c) >= 97 and ord(c) <= 122
-
