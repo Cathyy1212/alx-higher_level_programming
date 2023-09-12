891dadfe07baee9fe455f68350ffd64982f01568
Cathyy1212 <khadijahissou2@gmail.com>
2023-09-12 15:21:41 +0000

    0x03py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # 7-add_tuple.py
-
-
-def add_tuple(tuple_a=(), tuple_b=()):
+dd_tuple(tuple_a=(), tuple_b=()):
 	"""Add two tuples."""
 	if len(tuple_a) < 2:
 	if len(tuple_a) == 0:
@@ -13,6 +11,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
 	if len(tuple_b) == 0:
 		tuple_b = 0, 0
 	else:
-		tuple_b = tuple_b[0], 0
+	tuple_b = tuple_b[0], 0
 
 	return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
