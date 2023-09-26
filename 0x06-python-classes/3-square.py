6437f6766321310706041168b3b0e8f1747c1cb4
Cathyy1212 <khadijahissou2@gmail.com>
2023-09-26 23:10:16 +0000

    0x06py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
- 
-class Square."""
+"""Define a class Square."""
 
 
 class Square:
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
-    def __init__(self, size=0):
-       """Initialize a new square.
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    def __init__(self, side_length=0):
+        """Initialize a new square.
+
+        Args:
+            side_length (int): The side length of the new square.
+        """
+        self.side_length = side_length
+
+    @property
+    def side_length(self):
+        """Get/set the current side length of the square."""
+        return self.__side_length
+
+    @side_length.setter
+    def side_length(self, value):
+        if not isinstance(value, int):
+            raise TypeError("side_length must be an integer")
+        elif value < 0:
+            raise ValueError("side_length must be >= 0")
+        self.__side_length = value
 
     def area(self):
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return self.__side_length * self.__side_length
