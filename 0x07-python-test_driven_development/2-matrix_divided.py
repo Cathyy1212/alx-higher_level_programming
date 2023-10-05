c13936cf4425ec8cc0e2160a91d3a72b7a67541f
Cathyy1212 <khadijahissou2@gmail.com>
2023-10-05 08:24:35 -0700

    0x07py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,60 +5,39 @@
 """Defines a matrix division function."""
 
 
-
 def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix.
 
-    """Divide all elements of a matrix.
+    Args:
+        matrix (list of lists): The matrix containing integers or floats.
+        div (int or float): The divisor.
 
+    Returns:
+        list of lists: A new matrix with elements divided by div, rounded to 2 decimal places.
 
-    Args:
+    Raises:
+        TypeError: If the matrix is not a list of lists of integers or floats,
+                   or if rows have different sizes.
+        TypeError: If div is not a number (integer or float).
+        ZeroDivisionError: If div is equal to 0.
 
-        matrix (list): A list of lists of ints or floats.
+    Examples:
+        >>> matrix = [[1, 2, 3], [4, 5, 6]]
+        >>> matrix_divided(matrix, 3)
+        [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+    """
+    if not all(isinstance(row, list) and all(isinstance(x, (int, float)) for x in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
-        div (int/float): The divisor.
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
 
-    Raises:
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-        TypeError: If the matrix contains non-numbers.
+    return [[round(x / div, 2) for x in row] for row in matrix]
 
-        TypeError: If the matrix contains rows of different sizes.
-
-        TypeError: If div is not an int or float.
-
-        ZeroDivisionError: If div is 0.
-
-    Returns:
-
-        A new matrix representing the result of the division.
-
-    """
-
-    if (not isinstance(matrix, list) or matrix == [] or
-
-            not all(isinstance(row, list) for row in matrix) or
-
-            not all((isinstance(ele, int) or isinstance(ele, float))
-
-                    for ele in [num for row in matrix for num in row])):
-
-        raise TypeError("matrix must be a matrix (list of lists) of "
-
-                        "integers/floats")
-
-
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-
-        raise TypeError("Each row of the matrix must have the same size")
-
-
-    if not isinstance(div, int) and not isinstance(div, float):
-
-        raise TypeError("div must be a number")
-
-
-    if div == 0:
-
-        raise ZeroDivisionError("division by zero")
-
-
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,30 +5,24 @@
 """Defines a name-printing function."""
 
 
-
 def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first name> <last name>".
 
-    """Print a name.
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to an empty string.
 
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
 
-    Args:
+    Examples:
+        >>> say_my_name("John", "Doe")
+        My name is John Doe
+        >>> say_my_name("Jane")
+        My name is Jane 
+    """
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError("first_name must be a string and last_name must be a string")
 
-        first_name (str): The first name to print.
-
-        last_name (str): The last name to print.
-
-    Raises:
-
-        TypeError: If either of first_name or last_name are not strings.
-
-    """
-
-    if not isinstance(first_name, str):
-
-        raise TypeError("first_name must be a string")
-
-    if not isinstance(last_name, str):
-
-        raise TypeError("last_name must be a string")
-
-    print("My name is {} {}".format(first_name, last_name))
+    print(f"My name is {first_name} {last_name}")
